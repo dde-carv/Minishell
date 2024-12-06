456dfcd427a951b186b31777085564bbbfe6ccb3
dde-carv <duarteeusebio@Duartes-MacBook-Air.local>
2024-12-06 15:01:59 +0000

    Bora

--- src/main_msl/main.c
+++ src/main_msl/main.c
@@ -6,7 +6,7 @@
 /*   By: dde-carv <dde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 14:09:07 by dde-carv          #+#    #+#             */
-/*   Updated: 2024/12/06 12:01:42 by dde-carv         ###   ########.fr       */
+/*   Updated: 2024/12/06 14:20:34 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,33 +34,40 @@ static char	*ft_getprompt(char **env) //function to get the prompt using env var
 	return (promp[i - 1]);
 }
 
-static void	start_pipex(char *line, char **env) //function to initialize pipex
+/* static void	start_pipex(char *line, char **env) //function to initialize pipex
 {
-	char	**cmd
-}
+	char	**line_split;
+	int		i;
+
+	i = 0;
+	line_split = ft_split(line, ' ');
+} */
 
 int	main(int ac, char **av, char **env)
 {
 	(void)av;
 	char	*line;
+	char	*promp;
 
+	promp = ft_getprompt(env);
 	if (ac == 1)
 	{
 		while (1)
 		{
-			ft_printf("%s > ", ft_getprompt(env));
+			ft_printf("%s > ", promp);
 			line = get_next_line(0);
 			if (!ft_strncmp(line, "exit", 4) && (ft_strlen(line) - 1) == 4)
 			{
 				write(1, "exit\n", 5);
 				break;
 			}
-			start_minishell(line, env); //function to initialize minishell(not implemented)
+			// start_minishell(line, env); //function to initialize minishell(not implemented)
 			ft_printf("%s", line);
 			free (line);
-
+			free (promp);
 		}
-		//free(line);
+		free(line);
+		free(promp);
 	}
 	return (0);
 }
