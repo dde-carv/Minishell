72975b1d38a7d2fbe67c41d92ae5a3aba5241cf7
Luis Ribeiro <luiribei@student.42lisboa.com>
2025-01-13 12:07:57 +0000

    moved hashmap

---
+++ sources/hashmap/hash.c
@@ -0,0 +1,15 @@
+unsigned long hash(const char *key, unsigned int size)
+{
+    unsigned long   hash_value;
+    unsigned int    i;
+
+    hash_value = 0;
+    i = 0;
+    while (i < size)
+    {
+        hash_value += key[i];
+        hash_value = ((hash_value << 5) + hash_value) + key[i];
+        i++;
+    }
+    return (hash_value % size);
+}
