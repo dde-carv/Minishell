11fd96b78582c938a195b00d44b351ba45011ee3
Luis Ribeiro <luiribei@student.42lisboa.com>
2025-01-14 16:39:20 +0000

    setting hash for makefile

--- sources/hashmap/hash.c
+++ sources/hashmap/hash.c
@@ -14,24 +14,3 @@ unsigned long	hash_function(const char *key, unsigned int size)
 	}
 	return (hash_value % size);
 }
-
-int main()
-{
-	const char *keys[] = {"echo", "cd", "ls", "pwd", "grep"};
-	const char *values[] = {"Value1", "Value2", "Value3", "Value4", "Value5"};
-	unsigned int size = 1000; // Example size of the hash table
-	int i = 0;
-	t_hashmap *table = create_table(size);
-
-	while (i < 5)
-	{
-		unsigned long hash = hash_function(keys[i], size);
-		t_entry *entry = create_entry((char *)keys[i], (char *)values[i]);
-		table->entry[hash] = entry;
-		printf("Inserted key %s:\t %s at index %lu\n", keys[i], values[i], hash);
-		i++;
-	}
-	return 0;
-}
-
-// clear && cc -Wall -Wextra -Werror -o hash_test hash.c create_entry.c create_table.c ../../libft/libft.a && ./hash_test && rm hash_test
---
+++ sources/hashmap/hashmap_delete.c
@@ -0,0 +1,12 @@
+#include "../../includes/minishell.h"
+
+/* static void	find_and_delete_node_then_relink_list(t_hashmap *table,
+			t_entry *current, char *key)
+{
+	t_entry	*tmp;
+
+	while (current->next != NULL)
+	{
+		if (ft_strncmp(current->next->key, key, ft_strlen(key)))
+	}
+} */
