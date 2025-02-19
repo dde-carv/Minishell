78ccc6bb3d3236748b773f324620d577a73bcc91
Duarte Eusébio <dde-carv@student.42lisboa.com>
2025-02-19 16:43:31 +0000

    On the jobs.

--- sources/exec/fds.c
+++ sources/exec/fds.c
@@ -8,6 +8,22 @@ int	handle_fd(t_input **input)
 	while (fd)
 	{
 		take_quotes(&fd->file_n);
-		if (fd->type == )
+		if (fd->type == TRUNCATE)
+			fd->fd = open(fd->file_n, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+		if (fd->type == APPEND)
+			fd->fd = open(fd->file_n, O_CREAT | O_APPEND | O_WRONLY, 0664);
+		if (fd->type == REVERSE)
+			fd->fd = open(fd->file_n, O_RDONLY, 0644);
+		if (fd->type == HEREDOC)
+			fd->fd = 42000;
+		if (fd->fd == -1)
+			break ;
+		fd = fd->next;
 	}
+	if (fd && fd->fd == -1)
+	{
+		error_mess(fd->file_n, NO_FILE_OR_DIR, 1);
+		return (0);
+	}
+	return (1);
 }
