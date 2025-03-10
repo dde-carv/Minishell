4a960d0467cf456d66ed95851f0c553a56d3f363
Duarte Eusébio <dde-carv@student.42lisboa.com>
2025-03-10 09:29:31 +0000

    On the works.

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: dde-carv <dde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 11:18:46 by dde-carv          #+#    #+#             */
-/*   Updated: 2025/03/03 13:14:36 by dde-carv         ###   ########.fr       */
+/*   Updated: 2025/03/10 09:19:54 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,7 @@
 
 typedef struct s_pipe
 {
-	int		argc;
-	int		(*fd)[2];
-	int		heredoc_pid;
-	int		*pids;
-	char	**envp;
-	char	**env_paths;
-	char	**cmd_paths;
+	
 
 }		t_pipe;
 
